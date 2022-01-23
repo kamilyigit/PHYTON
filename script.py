e72e437f2c5ca9a827ce83d4083951f950ceff71
KamilYigit <Ygt.gultekin@gmail.com>
2022-01-23 12:33:24 +0100

    initial commit

---
+++ script.py
@@ -0,0 +1,19 @@
+import math
+import os
+import sys
+
+import requests
+
+print (sys.version)
+print(sys.executable)
+
+
+def greet(who_to_greet):
+    greeting = 'hello,{}'.format(who_to_greet)
+    return greeting
+
+name=input('what')
+print('hello',name)
+
+
+
