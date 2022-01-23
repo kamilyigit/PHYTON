99133ab29c00d4130e2c82192988e67373c9bdb1
KamilYigit <Ygt.gultekin@gmail.com>
2022-01-23 12:39:15 +0100

    removed some code

--- script.py
+++ script.py
@@ -1,16 +1,8 @@
-import math
-import os
-import sys
 
 import requests
 
-print (sys.version)
-print(sys.executable)
 
 
-def greet(who_to_greet):
-    greeting = 'hello,{}'.format(who_to_greet)
-    return greeting
 
 name=input('what')
 print('hello',name)
